2c31baaaff295c4d6ca9b5eaa6e26597c781efc5
BrunoFelix <brunofelixbarbosa123@hotmail.com>
2017-05-12 21:04:54 -0300

    Criação da classe básica categoria.

---
+++ Código/Restoran/src/Basica/Categoria.java
@@ -0,0 +1,36 @@
+package Basica;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id @GeneratedValue
+	private int id_categoria;
+	
+	@Column(length=50, nullable = false)
+	private String nome;
+	
+	//Gets & Sets
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+  
+}
