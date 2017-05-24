025886bd8b548652f9523bb6a0567a33f9bfc07a
Maikon Silva de Oliveira <maikon.silva@hotmail.com.br>
2017-05-24 20:17:41 -0300

    Update RNCategoria.java

--- Código/Restoran/src/Negocio/RNCategoria.java
+++ Código/Restoran/src/Negocio/RNCategoria.java
@@ -2,5 +2,35 @@ package Negocio;
 
 public class RNCategoria {
 	
+	CategoriaDAO categoriaDAO;
+
+	public RNCategoria() {
+
+	  categoriaDAO = new CategoriaDAO();
+	}
+	
+    /*##################################
+              FUNCIONALIDADES
+    ##################################*/
+    
+    public void salvar(Categoria c) throws NegocioException, DadosException{
+    	
+         salvar(c);
+       
+    }
+    
+    public void alterar(Categoria c) throws ControladorException{
+    	
+        alterar(c);
+
+    }
+    
+    public void excluir(Categoria c) throws ControladorException{
+       
+        excluir(c);
+    }
+    /*##################################
+              VALIDAÇÕES
+    ##################################*/	
 
 }
