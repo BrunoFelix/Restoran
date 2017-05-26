544fc42c6c8b96383ea84af71d7851ddc65e310d
TJBorges <tj.batera1@gmail.com>
2017-05-26 19:52:15 -0300

    RN Categoria "Pronto"

--- Código/Restoran/src/Negocio/RNCategoria.java
+++ Código/Restoran/src/Negocio/RNCategoria.java
@@ -12,31 +12,73 @@ public class RNCategoria {
 
 	public RNCategoria() {
 
-	  categoriaDAO = new CategoriaDAO();
+		categoriaDAO = new CategoriaDAO();
+	}
+
+	/*
+	 * ################################## 
+	 * 			FUNCIONALIDADES
+	 * ##################################
+	 */
+
+	public void salvar(Categoria c) throws NegocioException, DadosException {
+		validarCampos(c);
+		validarDuplicidadeCategoria(c);
+		categoriaDAO.insert(c);
+
+	}
+
+	public void alterar(Categoria c) throws ControladorException, NegocioException, DadosException {
+		validarCampos(c);
+		validarDuplicidadeCategoria(c);
+		categoriaDAO.update(c);
+	}
+
+	public void excluir(Categoria c) throws ControladorException, NegocioException, DadosException {
+		validaExistencia(c);
+		categoriaDAO.remove(c);
 	}
 	
-    /*##################################
-              FUNCIONALIDADES
-    ##################################*/
-    
-    public void salvar(Categoria c) throws NegocioException, DadosException{
-    	
-         salvar(c);
-       
-    }
-    
-    public void alterar(Categoria c) throws ControladorException{
-    	
-        alterar(c);
+	
+	/*
+	 * ################################## 
+	 * 				VALIDAÇÕES
+	 * ##################################
+	 */
+	public void verificarObjeto(Categoria c) throws NegocioException {
 
-    }
-    
-    public void excluir(Categoria c) throws ControladorException{
-       
-        excluir(c);
-    }
-    /*##################################
-              VALIDAÃ‡Ã•ES
-    ##################################*/	
+		if (c == null)
+			throw new NegocioException("Objeto Categoria não preenchido");
+	}
+
+	public void validarCampos(Categoria c) throws NegocioException {
+		if ((c.getNome().isEmpty() == true) || (c.getNome()).length() < 2)
+			throw new NegocioException("Nome Inválido");
+		
+	}
+
+	public void validarDuplicidadeCategoria(Categoria c) throws NegocioException, DadosException {
+
+		try {
+			if (categoriaDAO.PesquisarPorNome(c.getNome()) != null) {
+				throw new NegocioException("Categoria já existe");
+			}
+		} catch (DadosException e) {
+			throw new DadosException("Banco de dados não disponivel");
+		}
+	}
+
+
+	public void validaExistencia(Categoria c) throws NegocioException, DadosException {
+
+		try {
+			if (categoriaDAO.PesquisarPorNome(c.getNome()) == null) {
+				throw new NegocioException("Categoria que deseja excluir não existe");
+			}
+		} catch (DadosException e) {
+			throw new DadosException("Banco de dados não disponivel");
+		}
+	}
+	
 
 }
