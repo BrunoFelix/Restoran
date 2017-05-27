f88ea19a2bbb210c2c177efe83a572fc906bf867
CAMPUS\aluno <maikon.silva@hotmail.com.br>
2017-05-26 21:24:11 -0300

    Ajuste estrutura do codigo

--- Código/Restoran/src/Negocio/RNHistorico.java
+++ Código/Restoran/src/Negocio/RNHistorico.java
@@ -1,5 +1,22 @@
 package Negocio;
 
+
+import Dados.HistoricoDAO;
+
+
 public class RNHistorico {
+	
+	HistoricoDAO rnhistoricoDAO;
+
+	public RNHistorico() {
+
+		rnhistoricoDAO = new HistoricoDAO();
+	}
+
+	/*
+	 * ################################## 
+	 * 			FUNCIONALIDADES
+	 * ##################################
+	 */
 
 }
